3df304432bbfda5a3612b2a7c20959dd6f8e76a8
Piotrek <p.popowski01@gmail.com>
2019-10-19 15:52:55 +0200

    App1

---
+++ App1/MainActivity.cs
@@ -0,0 +1,171 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using App1.Modules;
+using Xamarin.Essentials;
+using System.Security;
+using Android.Views;
+using System;
+using System.Linq;
+using Android.Content;
+using Android.Runtime;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private SecureString passwordInMemory = new SecureString();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (string.IsNullOrWhiteSpace(SecureStorage.GetAsync("user_password").Result))
+                SetContentView(Resource.Layout.activity_main);
+            else
+                SetContentView(Resource.Layout.encryptedLayout);
+        }
+
+        protected override void OnPause()
+        {
+            if (string.IsNullOrWhiteSpace(SecureStorage.GetAsync("user_password").Result))
+                SetContentView(Resource.Layout.activity_main);
+            else
+                SetContentView(Resource.Layout.encryptedLayout);
+            base.OnPause();
+        }
+
+        [Java.Interop.Export("Create")]
+        public void Create(View v)
+        {
+            var notepad = FindViewById<EditText>(Resource.Id.notepad);
+            var notepadPassword = FindViewById<EditText>(Resource.Id.password);
+            var notepadRepeatPassword = FindViewById<EditText>(Resource.Id.repeatPassword);
+
+            if (notepadPassword.Text != notepadRepeatPassword.Text)
+            {
+                setAlert("Given passwords are different.");
+                return;
+            }
+            try
+            {
+                savePassword(notepadPassword.Text);
+            }catch(Exception e)
+            {
+                setAlert(e.Message);
+                return;
+            }
+            encryptNotepad(notepadPassword.Text, notepad.Text);
+
+            SetContentView(Resource.Layout.encryptedLayout);
+        }
+
+        [Java.Interop.Export("EncryptText")]
+        public void EncryptText(View v)
+        {
+            var notepad = FindViewById<EditText>(Resource.Id.notepad);
+            encryptNotepad(new System.Net.NetworkCredential("",passwordInMemory).Password, notepad.Text);
+
+            passwordInMemory.Clear();
+            SetContentView(Resource.Layout.encryptedLayout);
+        }
+
+        [Java.Interop.Export("DecryptText")]
+        public void DecryptText(View v)
+        {
+            var password = FindViewById<EditText>(Resource.Id.password);
+            var pswd = password.Text;
+            if(String.IsNullOrWhiteSpace(pswd))
+            {
+                return;
+            }
+            var hash = Encrypter.GetHash(pswd, SecureStorage.GetAsync("user_salt").Result);
+            if(hash != SecureStorage.GetAsync("user_password").Result)
+            {
+                setAlert("Incorrect password.");
+            }
+            else
+            {
+                password.Text.ToCharArray().ToList()
+                    .ForEach(c => passwordInMemory.AppendChar(c));
+                SetContentView(Resource.Layout.decryptedLayout);
+                var notepad = FindViewById<EditText>(Resource.Id.notepad);
+                notepad.Text = StringCipher.Decrypt(Storage.Get("notepad"), pswd);
+            }
+        }
+
+        [Java.Interop.Export("ChangePassword")]
+        public void ChangePassword(View v)
+        {
+            var password = FindViewById<EditText>(Resource.Id.password);
+            var repeatPassword = FindViewById<EditText>(Resource.Id.repeatPassword);
+
+            if(password.Text != repeatPassword.Text)
+            {
+                setAlert("Given passwords are different.");
+                return;
+            }
+
+            try
+            {
+                savePassword(password.Text);
+            }catch(Exception e)
+            {
+                setAlert(e.Message);
+                return;
+            }
+
+            var notepadText = StringCipher.Decrypt(Storage.Get("notepad"), 
+                                new System.Net.NetworkCredential("", passwordInMemory).Password);
+            encryptNotepad(password.Text, notepadText);
+            
+            passwordInMemory.Clear();
+            password.Text.ToCharArray().ToList()
+                .ForEach(c => passwordInMemory.AppendChar(c));
+
+            setAlert("Password successfully changed.", "green");
+            password.Text = "";
+            repeatPassword.Text = "";
+        }
+
+        [Java.Interop.Export("SingOut")]
+        public void SingOut(View v)
+        {
+            var password = FindViewById<EditText>(Resource.Id.password);
+            var repeatPassword = FindViewById<EditText>(Resource.Id.repeatPassword);
+            var notepad = FindViewById<EditText>(Resource.Id.notepad);
+            
+            passwordInMemory.Clear();
+
+            SetContentView(Resource.Layout.encryptedLayout);
+        }
+
+        private void encryptNotepad(string password, string text)
+        {
+            var encryptedText = StringCipher.Encrypt(text, password);
+            Storage.Save("notepad", encryptedText);
+        }
+
+        private void savePassword(string password)
+        {
+            if(password.Length<4)
+            {
+                throw new Exception("Password has to contain at least 4 characters.");
+            }
+
+            var salt = Encrypter.GenerateSalt();
+            var hash = Encrypter.GetHash(password, salt);
+
+            SecureStorage.SetAsync("user_salt", salt).Wait();
+            SecureStorage.SetAsync("user_password", hash).Wait();
+        }
+
+        private void setAlert(string text, string color="red")
+        {
+            var alert = FindViewById<TextView>(Resource.Id.alertText);
+            alert.SetTextColor(Android.Graphics.Color.ParseColor(color));
+            alert.Text = text;
+        }
+    }
+}
---
+++ App1/Modules/Storage.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace App1.Modules
+{
+    public static class Storage
+    {
+        public static void Save(string key, string value)
+        {
+            var prefs = Application.Context.GetSharedPreferences("App1", FileCreationMode.Private);
+            var prefEditor = prefs.Edit();
+            prefEditor.PutString(key, value);
+            prefEditor.Commit();
+        }
+
+        public static string Get(string key)
+        {
+            //retreive 
+            var prefs = Application.Context.GetSharedPreferences("App1", FileCreationMode.Private);
+            return prefs.GetString(key, null);
+        }
+    }
+}
